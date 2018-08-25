c59395368b1e9329939313e31f34d6bf22e626fe
Aleksbgbg <kozichka01@abv.bg>
2018-08-25 18:31:25 +0100

    [RestEndpoint] Add RestEndpoint to Endpoints namespace, with disposable pattern.

---
+++ Server/Endpoints/RestEndpoint.cs
@@ -0,0 +1,26 @@
+﻿namespace Skype2.Server.Endpoints
+{
+    using System;
+
+    internal class RestEndpoint : IDisposable
+    {
+        ~RestEndpoint()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+
+            }
+        }
+    }
+}
