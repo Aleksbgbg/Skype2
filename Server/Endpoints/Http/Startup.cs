d10a3550814c59ec3877a684b9ac25df803c67cb
Aleksbgbg <kozichka01@abv.bg>
2018-08-25 18:38:55 +0100

    [Startup] Add WebAPI Startup module.

---
+++ Server/Endpoints/Http/Startup.cs
@@ -0,0 +1,22 @@
+﻿namespace Skype2.Server.Endpoints.Http
+{
+    using System;
+    using System.Net.Http.Formatting;
+    using System.Web.Http;
+
+    using Owin;
+
+    internal class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            config.MapHttpAttributeRoutes();
+
+            config.Formatters.JsonFormatter.MediaTypeMappings.Add(new RequestHeaderMapping("Accept", "text/html", StringComparison.InvariantCultureIgnoreCase, true, "application/json"));
+
+            appBuilder.UseWebApi(config);
+        }
+    } 
+}
