ba0db7e8d2c36740a293583d59f7240299be3c25
Aleksbgbg <kozichka01@abv.bg>
2018-08-25 15:02:54 +0100

    [User] Add user model.

---
+++ Server Client Shared/Models/User.cs
@@ -0,0 +1,20 @@
+﻿namespace Shared.Models
+{
+    using Newtonsoft.Json;
+
+    public class User
+    {
+        [JsonConstructor]
+        public User(ulong id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        [JsonProperty("Id")]
+        public ulong Id { get; }
+
+        [JsonProperty("Id")]
+        public string Name { get; }
+    }
+}
