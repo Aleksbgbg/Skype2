b7789bb624cb64d2b27de297519e2edd5ea31122
Aleksbgbg <kozichka01@abv.bg>
2018-08-25 19:29:04 +0100

    [MessagesController] Add controller for managing messages. Currently returns an empty array when retrieving all messages.

---
+++ Server/Endpoints/Http/Controllers/MessagesController.cs
@@ -0,0 +1,17 @@
+﻿namespace Skype2.Server.Endpoints.Http.Controllers
+{
+    using System.Web.Http;
+
+    using Shared.Models;
+
+    [RoutePrefix("messages")]
+    public class MessagesController : ApiController
+    {
+        [HttpGet]
+        [Route("get/all")]
+        public Message[] GetAllMessages()
+        {
+            return new Message[0];
+        }
+    }
+}
