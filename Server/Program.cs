4d6fdcd0b168b229e0e21e062881b300cee5d23c
Aleksbgbg <kozichka01@abv.bg>
2018-08-25 18:31:39 +0100

    [Program] Run RestEndpoint.

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,9 +1,17 @@
 ﻿namespace Skype2.Server
 {
+    using System;
+
+    using Skype2.Server.Endpoints;
+
     internal static class Program
     {
         private static void Main()
         {
+            using (RestEndpoint restEndpoint = new RestEndpoint())
+            {
+                Console.ReadKey();
+            }
         }
     }
 }
