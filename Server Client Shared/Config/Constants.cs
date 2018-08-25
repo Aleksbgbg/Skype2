d0e2f150a0b97877fed831acf77a006545cf5b7b
Aleksbgbg <kozichka01@abv.bg>
2018-08-25 18:40:39 +0100

    [Constants] Add class which contains server/client constants such as the server's IP, and the ports on which each endpoint is hosted.

---
+++ Server Client Shared/Config/Constants.cs
@@ -0,0 +1,13 @@
+﻿namespace Shared.Config
+{
+    using System.Net;
+
+    public static class Constants
+    {
+        public static IPAddress ServerIp = IPAddress.Parse("81.107.155.88");
+
+        public const int TcpPort = 3001;
+
+        public const int HttpPort = 3002;
+    }
+}
