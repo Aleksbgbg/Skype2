38f4e89cc044d8a4d67abb03f7d4167e8182daf8
Aleksbgbg <kozichka01@abv.bg>
2018-08-25 15:08:34 +0100

    [Message] Add message model.

---
+++ Server Client Shared/Models/Message.cs
@@ -0,0 +1,20 @@
+﻿namespace Shared.Models
+{
+    using System;
+
+    public class Message
+    {
+        public Message(User sender, DateTime createdAt, string content)
+        {
+            Sender = sender;
+            CreatedAt = createdAt;
+            Content = content;
+        }
+
+        public User Sender { get; }
+
+        public DateTime CreatedAt { get; }
+
+        public string Content { get; }
+    }
+}
