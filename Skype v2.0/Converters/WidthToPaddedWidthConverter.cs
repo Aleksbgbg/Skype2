12201ae5fb5d5ec86ec3dd6e8fb35574e5b14199
Aleksbgbg <kozichka01@abv.bg>
2018-08-26 19:52:18 +0100

    [WidthToPaddedWidthConverter] Add converter which simply subtracts 60 from the specified width.

---
+++ Skype v2.0/Converters/WidthToPaddedWidthConverter.cs
@@ -0,0 +1,23 @@
+﻿namespace Skype2.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    [ValueConversion(typeof(double), typeof(double))]
+    internal class WidthToPaddedWidthConverter : IValueConverter
+    {
+        public static WidthToPaddedWidthConverter Default { get; } = new WidthToPaddedWidthConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (double)value - 60.0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+            return (double)value + (double)parameter;
+        }
+    }
+}
