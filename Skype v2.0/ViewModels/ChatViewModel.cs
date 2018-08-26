0e14ff54c1ffdeffdbfaab9ec9bfea92bea30593
Aleksbgbg <kozichka01@abv.bg>
2018-08-26 19:31:05 +0100

    [ChatViewModel] Use RestService to load all messages on a separate thread at startup. Also implement an 'AddMessage' method which adds messages to the right cluster based on the sender.

--- Skype v2.0/ViewModels/ChatViewModel.cs
+++ Skype v2.0/ViewModels/ChatViewModel.cs
@@ -1,11 +1,52 @@
 ﻿namespace Skype2.ViewModels
 {
+    using System.Linq;
+    using System.Threading.Tasks;
+
     using Caliburn.Micro;
 
+    using Skype2.Factories.Interfaces;
+    using Skype2.Services.Interfaces;
     using Skype2.ViewModels.Interfaces;
+
+    using Message = Shared.Models.Message;
 
     internal class ChatViewModel : ViewModelBase, IChatViewModel
     {
+        private readonly IMessageFactory _messageFactory;
+
+        private readonly IRestService _restService;
+
+        public ChatViewModel(IMessageFactory messageFactory, IRestService restService)
+        {
+            _messageFactory = messageFactory;
+            _restService = restService;
+
+            Task.Run(LoadMessages);
+        }
+
         public IObservableCollection<IMessageClusterViewModel> MessageClusters { get; } = new BindableCollection<IMessageClusterViewModel>();
+
+        private async Task LoadMessages()
+        {
+            foreach (Message message in await _restService.GetMessages())
+            {
+                AddMessage(message);
+            }
+        }
+
+        private void AddMessage(Message message)
+        {
+            IMessageClusterViewModel lastMessageCluster = MessageClusters.LastOrDefault();
+
+            if (lastMessageCluster == null || lastMessageCluster.RootMessage.Message.Sender != message.Sender)
+            {
+                MessageClusters.Add(_messageFactory.MakeMessageClusterViewModel(message));
+            }
+            else
+            {
+                lastMessageCluster.AddMessage(message);
+            }
+        }
     }
 }
