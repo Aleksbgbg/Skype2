99b6ee45ed006305b722c644abeb620c2ba4c4d3
Aleksbgbg <kozichka01@abv.bg>
2018-08-27 20:49:08 +0100

    [MessageService] Add service which allows sending and receiving of messages via a TCP connection to the TCP server.

---
+++ Skype v2.0/Services/Interfaces/IMessageService.cs
@@ -0,0 +1,15 @@
+﻿namespace Skype2.Services.Interfaces
+{
+    using System;
+
+    using Skype2.EventArgs;
+
+    internal interface IMessageService
+    {
+        event EventHandler<MessageReceivedEventArgs> MessageReceived;
+
+        void SendMessage(byte[] content);
+
+        void SendMessage(string content);
+    }
+}
---
+++ Skype v2.0/Services/MessageService.cs
@@ -0,0 +1,33 @@
+﻿namespace Skype2.Services
+{
+    using System;
+
+    using Shared.Config;
+
+    using SimpleTCP;
+
+    using Skype2.EventArgs;
+    using Skype2.Services.Interfaces;
+
+    internal class MessageService : IMessageService
+    {
+        private readonly SimpleTcpClient _tcpClient = new SimpleTcpClient().Connect(Constants.ServerIp.ToString(), Constants.TcpPort);
+
+        public MessageService()
+        {
+            _tcpClient.DataReceived += (sender, e) => MessageReceived?.Invoke(this, new MessageReceivedEventArgs(e.Data, e.MessageString));
+        }
+
+        public event EventHandler<MessageReceivedEventArgs> MessageReceived;
+
+        public void SendMessage(byte[] content)
+        {
+            _tcpClient.Write(content);
+        }
+
+        public void SendMessage(string content)
+        {
+            _tcpClient.Write(content);
+        }
+    }
+}
