62c19b4c7c595b9b993c1bef52bc91473bce2109
Aleksbgbg <kozichka01@abv.bg>
2018-08-27 18:36:21 +0100

    [ImageIdToImageSourceConverter] Add converter to convert from an image ID to an ImageSource object. Also caches images.

---
+++ Skype v2.0/Converters/ImageIdToImageSourceConverter.cs
@@ -0,0 +1,40 @@
+﻿namespace Skype2.Converters
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Windows.Data;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+
+    using Shared.Config;
+
+    [ValueConversion(typeof(long), typeof(ImageSource))]
+    internal class ImageIdToImageSourceConverter : IValueConverter
+    {
+        private readonly Dictionary<long, ImageSource> _imageCache = new Dictionary<long, ImageSource>();
+
+        public static ImageIdToImageSourceConverter Default { get; } = new ImageIdToImageSourceConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            long imageId = (long)value;
+
+            if (_imageCache.TryGetValue(imageId, out ImageSource cachedImage))
+            {
+                return cachedImage;
+            }
+
+            ImageSource imageSource = new BitmapImage(new Uri($"http://{Constants.ServerIp}:{Constants.HttpPort}/images/get/{imageId}"));
+
+            _imageCache[imageId] = imageSource;
+
+            return imageSource;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException($"ConvertBack is not supported on {nameof(ImageIdToImageSourceConverter)}.");
+        }
+    }
+}
