0c9fa25e6ab9b9edc0f6b639dad432d0cbb85da6
Aleksbgbg <kozichka01@abv.bg>
2018-08-27 20:43:17 +0100

    [MessageReceivedEventArgs] Add arguments for receiving messages.

---
+++ Skype v2.0/EventArgs/MessageReceivedEventArgs.cs
@@ -0,0 +1,17 @@
+﻿namespace Skype2.EventArgs
+{
+    using System;
+
+    internal class MessageReceivedEventArgs : EventArgs
+    {
+        public MessageReceivedEventArgs(byte[] data, string messageString)
+        {
+            Data = data;
+            MessageString = messageString;
+        }
+
+        public byte[] Data { get; }
+
+        public string MessageString { get; }
+    }
+}
