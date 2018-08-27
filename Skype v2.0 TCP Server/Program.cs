5b4e01f076dd4a005dd7f839e4ed12e5edded86d
Aleksbgbg <kozichka01@abv.bg>
2018-08-27 20:35:22 +0100

    [Solution] Add 'TCP Server' project with several packages. Include a simple server initialisation, with message forwarding, and logging messages to the console.

---
+++ Skype v2.0 TCP Server/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace TcpServer
+{
+    using Shared.Config;
+
+    using SimpleTCP;
+
+    using static System.Console;
+
+    internal static class Program
+    {
+        private static void Main()
+        {
+            SimpleTcpServer tcpServer = new SimpleTcpServer().Start(Constants.TcpPort);
+
+            tcpServer.DataReceived += (sender, e) =>
+            {
+                tcpServer.Broadcast(e.Data);
+
+                WriteLine("Message: {0}", e.MessageString);
+            };
+
+            ReadKey();
+        }
+    }
+}
---
+++ Skype v2.0 TCP Server/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TCP Server")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TCP Server")]
+[assembly: AssemblyCopyright("Copyright © Aleks Todorov 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("0b82da5b-e0f3-46d8-b25a-90c53e55d02a")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
