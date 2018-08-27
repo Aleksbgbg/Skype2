31d1a0002bb7a83d6c0841d062d644f4f8b7a284
Aleksbgbg <kozichka01@abv.bg>
2018-08-27 21:47:34 +0100

    [AppBootstrapper] Register MessageService.

--- Skype v2.0/AppBootstrapper.cs
+++ Skype v2.0/AppBootstrapper.cs
@@ -35,6 +35,7 @@
             // Register Services
             _container.Singleton<IWindowManager, WindowManager>();
 
+            _container.Singleton<IMessageService, MessageService>();
             _container.Singleton<IRestService, RestService>();
             _container.Singleton<IUserService, UserService>();
 
