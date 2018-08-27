9c7f041c0f1486f54a29ecf379e24f6f653033fe
Aleksbgbg <kozichka01@abv.bg>
2018-08-27 18:32:59 +0100

    [Skype2Context] Update database model to match new 'user_image' database table.

--- Skype v2.0 HTTP Server/Database/Skype2Context.cs
+++ Skype v2.0 HTTP Server/Database/Skype2Context.cs
@@ -14,6 +14,8 @@
 
         public virtual DbSet<User> Users { get; protected set; }
 
+        public virtual DbSet<UserImage> UserImages { get; protected set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Message>(entity =>
@@ -52,14 +54,39 @@
                       .HasColumnName("id")
                       .HasDefaultValueSql("shard_1.id_generator()");
 
+                entity.Property(e => e.ImageId)
+                      .HasColumnName("image_id")
+                      .HasDefaultValueSql("'1855266345831107587'::bigint");
+
                 entity.Property(e => e.Name)
                       .IsRequired()
                       .HasColumnName("name")
                       .HasDefaultValueSql("'NewUser'::text");
+
+                entity.HasOne(d => d.Image)
+                      .WithMany(p => p.AttachedUsers)
+                      .HasForeignKey(d => d.ImageId)
+                      .OnDelete(DeleteBehavior.ClientSetNull)
+                      .HasConstraintName("image_reference");
+            });
+
+            modelBuilder.Entity<UserImage>(entity =>
+            {
+                entity.ToTable("user_image");
+
+                entity.Property(e => e.Id)
+                      .HasColumnName("id")
+                      .HasDefaultValueSql("shard_3.id_generator()");
+
+                entity.Property(e => e.Extension)
+                      .IsRequired()
+                      .HasColumnName("extension");
             });
 
             modelBuilder.HasSequence("seq")
                         .HasMax(2147483647);
+
+            modelBuilder.HasSequence("global_id_sequence");
 
             modelBuilder.HasSequence("global_id_sequence");
 
