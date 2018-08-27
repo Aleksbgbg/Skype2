64371e64d24e61fbbfaaf5a172669bd69a1a4812
Aleksbgbg <kozichka01@abv.bg>
2018-08-27 18:31:35 +0100

    [User] Add 'ImageId' and 'Image' properties, representing the User's thumbnail image.

--- Server Client Shared/Models/User.cs
+++ Server Client Shared/Models/User.cs
@@ -9,6 +9,12 @@
         [JsonProperty("Name")]
         public string Name { get; set; }
 
+        [JsonProperty("ImageId")]
+        public long ImageId { get; set; }
+
+        [JsonIgnore]
+        public UserImage Image { get; set; }
+
         [JsonIgnore]
         public ICollection<Message> Messages { get; } = new HashSet<Message>();
     }
