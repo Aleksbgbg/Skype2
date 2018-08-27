8174aa86cc464f8d193d6b3bfb8ab7a749c8b68d
Aleksbgbg <kozichka01@abv.bg>
2018-08-27 18:31:01 +0100

    [UserImage] Add model for user images which have an 'Id' and 'Extension' properties, plus associated users. In addition, a 'Filename' property joins the ID and Extension to get a filename.

---
+++ Server Client Shared/Models/UserImage.cs
@@ -0,0 +1,18 @@
+﻿namespace Shared.Models
+{
+    using System.Collections.Generic;
+
+    using Newtonsoft.Json;
+
+    public class UserImage : Snowflake
+    {
+        [JsonProperty("Extension")]
+        public string Extension { get; set; }
+
+        [JsonIgnore]
+        public string Filename => string.Concat(Id, Extension);
+
+        [JsonIgnore]
+        public ICollection<User> AttachedUsers { get; } = new HashSet<User>();
+    }
+}
