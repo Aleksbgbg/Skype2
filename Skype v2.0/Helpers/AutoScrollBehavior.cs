201213e4d8220bef53c21eac5a5e573e9707a7e5
Aleksbgbg <kozichka01@abv.bg>
2018-08-28 12:28:20 +0100

    [AutoScrollBehavior] Add ScrollViewer behavior which automatically scrolls the bar to the bottom when the user is not manually scrolling, in the chat view.

---
+++ Skype v2.0/Helpers/AutoScrollBehavior.cs
@@ -0,0 +1,36 @@
+﻿namespace Skype2.Helpers
+{
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Interactivity;
+
+    internal class AutoScrollBehavior : Behavior<ScrollViewer>
+    {
+        private bool _isUserScrolling;
+
+        protected override void OnAttached()
+        {
+            AssociatedObject.ScrollChanged += AssociatedObject_ScrollChanged;
+            AssociatedObject.SizeChanged += AssociatedObject_SizeChanged;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.ScrollChanged -= AssociatedObject_ScrollChanged;
+            AssociatedObject.SizeChanged -= AssociatedObject_SizeChanged;
+        }
+
+        private void AssociatedObject_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (!_isUserScrolling)
+            {
+                AssociatedObject.ScrollToEnd();
+            }
+        }
+
+        private void AssociatedObject_ScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            _isUserScrolling = AssociatedObject.VerticalOffset != AssociatedObject.ScrollableHeight;
+        }
+    }
+}
