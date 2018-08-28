a3cdb75c6e866231707a81b21885bbd9ed4f2b2e
Aleksbgbg <kozichka01@abv.bg>
2018-08-28 11:48:00 +0100

    [UserController] Add controller which manages users, currently allowing retrieval of user images and uploading new images for users.

---
+++ Skype v2.0 HTTP Server/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿namespace HttpServer.Controllers
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using HttpServer.Database;
+    using HttpServer.Models;
+    using HttpServer.Services.Interfaces;
+
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("[Controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IImageService _imageService;
+
+        private readonly Skype2Context _databaseContext;
+
+        public UserController(IImageService imageService, Skype2Context databaseContext)
+        {
+            _imageService = imageService;
+            _databaseContext = databaseContext;
+        }
+
+        [HttpGet("{userId}/get/image")]
+        public ActionResult<IActionResult> GetImage(long userId)
+        {
+            ImageFileInfo imageFileInfo = _imageService.GetImage(_databaseContext.Users.Single(user => user.Id == userId).ImageId);
+
+            return PhysicalFile(imageFileInfo.Path, imageFileInfo.ContentType);
+        }
+
+        [HttpPost("{userId}/post/image")]
+        public async Task PostImage(long userId, IFormFile image)
+        {
+            await _imageService.UploadNewImageForUser(userId, image);
+        }
+    }
+}
