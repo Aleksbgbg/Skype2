758ab9c05deb25631c41d7ca17373785b28f6c90
Aleksbgbg <kozichka01@abv.bg>
2018-08-28 11:12:11 +0100

    [Program] POST messages sent to the server using HttpClient to save them to the database.

--- Skype v2.0 TCP Server/Program.cs
+++ Skype v2.0 TCP Server/Program.cs
@@ -1,5 +1,10 @@
 ﻿namespace TcpServer
 {
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Text;
+    using System.Threading.Tasks;
+
     using Newtonsoft.Json;
 
     using Shared.Config;
@@ -14,11 +19,18 @@
     {
         private static void Main()
         {
+            HttpClient httpClient = new HttpClient();
+
             SimpleTcpServer tcpServer = new SimpleTcpServer().Start(Constants.TcpPort);
 
-            tcpServer.DataReceived += (sender, e) =>
+            tcpServer.DataReceived += async (sender, e) =>
             {
                 tcpServer.Broadcast(e.Data);
+
+                ByteArrayContent byteArrayContent = new ByteArrayContent(Encoding.UTF8.GetBytes(e.MessageString));
+                byteArrayContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                await httpClient.PostAsync($"http://{Constants.ServerIp}:{Constants.HttpPort}/messages/post", byteArrayContent);
 
                 WriteLine("Message: {0}", JsonConvert.DeserializeObject<Message>(e.MessageString).Content);
             };
