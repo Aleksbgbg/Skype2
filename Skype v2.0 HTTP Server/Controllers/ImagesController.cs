37b1d5f509ce4c1c986c7cdfef6373cdfdcccebf
Aleksbgbg <kozichka01@abv.bg>
2018-08-28 11:43:30 +0100

    [ImagesController] Use ImageService for providing images. Remove PostImage method, as this will be implemented in a UserController.

--- Skype v2.0 HTTP Server/Controllers/ImagesController.cs
+++ Skype v2.0 HTTP Server/Controllers/ImagesController.cs
@@ -1,85 +1,27 @@
 ﻿namespace HttpServer.Controllers
 {
-    using System.IO;
-    using System.Linq;
-    using System.Threading.Tasks;
+    using HttpServer.Models;
+    using HttpServer.Services.Interfaces;
 
-    using HttpServer.Database;
-
-    using Microsoft.AspNetCore.Hosting;
-    using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
-    using Microsoft.AspNetCore.StaticFiles;
-    using Microsoft.EntityFrameworkCore;
-
-    using Shared.Models;
-
-    using IOFile = System.IO.File;
 
     [ApiController]
     [Route("[Controller]")]
     public class ImagesController : ControllerBase
     {
-        private const string UnknownContentType = "application/octet-stream";
+        private readonly IImageService _imageService;
 
-        private readonly IHostingEnvironment _environment;
-
-        private readonly Skype2Context _databaseContext;
-
-        private readonly FileExtensionContentTypeProvider _fileExtensionContentTypeProvider = new FileExtensionContentTypeProvider();
-
-        public ImagesController(IHostingEnvironment environment, Skype2Context databaseContext)
+        public ImagesController(IImageService imageService)
         {
-            _environment = environment;
-            _databaseContext = databaseContext;
+            _imageService = imageService;
         }
 
         [HttpGet("get/{id}")]
         public ActionResult<IActionResult> GetImage(long id)
         {
-            string imagePath = GetImagePath(_databaseContext.UserImages.First(image => image.Id == id));
+            ImageFileInfo imageFileInfo = _imageService.GetImage(id);
 
-            bool foundContentType = _fileExtensionContentTypeProvider.TryGetContentType(imagePath, out string contentType);
-
-            return PhysicalFile(imagePath, foundContentType ? contentType : UnknownContentType);
-        }
-
-        [HttpPost("post/user/{userId}")]
-        public async Task PostImage(long userId, IFormFile image)
-        {
-            UserImage newUserImage = new UserImage
-            {
-                Extension = Path.GetExtension(image.FileName)
-            };
-
-            // Register new image
-            await _databaseContext.UserImages.AddAsync(newUserImage);
-            await _databaseContext.SaveChangesAsync();
-
-            using (FileStream fileStream = IOFile.Create(GetImagePath(newUserImage)))
-            {
-                await image.CopyToAsync(fileStream);
-            }
-
-            User targetUser = _databaseContext.Users.Include(user => user.Image).First(user => user.Id == userId);
-
-            UserImage oldUserImage = targetUser.Image;
-
-            // Swap user image first, to ensure that foreign key constraint is not broken by removing the old image
-            targetUser.ImageId = newUserImage.Id;
-            await _databaseContext.SaveChangesAsync();
-
-            if (_databaseContext.UserImages.First().Id != oldUserImage.Id)
-            {
-                IOFile.Delete(GetImagePath(oldUserImage));
-                _databaseContext.UserImages.Remove(oldUserImage);
-                await _databaseContext.SaveChangesAsync();
-            }
-        }
-
-        private string GetImagePath(UserImage userImage)
-        {
-            return Path.Combine(_environment.WebRootPath, "User Images", userImage.Filename);
+            return PhysicalFile(imageFileInfo.Path, imageFileInfo.ContentType);
         }
     }
 }
