3e1b46d0cad8be3a3f0dddc920de0e0f97c4350f
Aleksbgbg <kozichka01@abv.bg>
2018-08-28 11:44:23 +0100

    [MessagesController] Stop including the message sender. Also remove unnecessary 'using' statements, and use CapitalCamelCase for controller route template.

--- Skype v2.0 HTTP Server/Controllers/MessagesController.cs
+++ Skype v2.0 HTTP Server/Controllers/MessagesController.cs
@@ -1,6 +1,5 @@
 ﻿namespace HttpServer.Controllers
 {
-    using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
@@ -8,12 +7,11 @@
     using HttpServer.Database;
 
     using Microsoft.AspNetCore.Mvc;
-    using Microsoft.EntityFrameworkCore;
 
     using Shared.Models;
 
     [ApiController]
-    [Route("[controller]")]
+    [Route("[Controller]")]
     public class MessagesController : ControllerBase
     {
         private readonly Skype2Context _database;
@@ -26,13 +24,13 @@
         [HttpGet("get/all")]
         public ActionResult<IEnumerable<Message>> GetMessages()
         {
-            return _database.Messages.Include(message => message.Sender).OrderBy(message => message.Id).ToArray();
+            return _database.Messages.OrderBy(message => message.Id).ToArray();
         }
 
         [HttpGet("get/{id}")]
         public ActionResult<Message> GetMessage(long id)
         {
-            return _database.Messages.Include(message => message.Sender).FirstOrDefault(message => message.Id == id);
+            return _database.Messages.FirstOrDefault(message => message.Id == id);
         }
 
         [HttpPost("post")]
