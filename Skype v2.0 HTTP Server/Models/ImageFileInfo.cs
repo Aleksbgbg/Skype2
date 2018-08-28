4fe5edc92cebc29ada671892ab568afa99304a12
Aleksbgbg <kozichka01@abv.bg>
2018-08-28 11:40:16 +0100

    [ImageFileInfo] Add model class to represent an image path and the content type that should be returned with a HTTP request.

---
+++ Skype v2.0 HTTP Server/Models/ImageFileInfo.cs
@@ -0,0 +1,15 @@
+﻿namespace HttpServer.Models
+{
+    public class ImageFileInfo
+    {
+        public ImageFileInfo(string path, string contentType)
+        {
+            Path = path;
+            ContentType = contentType;
+        }
+
+        public string Path { get; }
+
+        public string ContentType { get; }
+    }
+}
