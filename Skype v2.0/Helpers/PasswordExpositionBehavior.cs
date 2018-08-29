ed0289739109a30700919c687e073ff64587517f
Aleksbgbg <kozichka01@abv.bg>
2018-08-29 21:44:42 +0100

    [PasswordExpositionBehavior] Add behavior to expose the password property on the PasswordBox via a SecureString.

---
+++ Skype v2.0/Helpers/PasswordExpositionBehavior.cs
@@ -0,0 +1,35 @@
+﻿namespace Skype2.Helpers
+{
+    using System.Security;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Interactivity;
+
+    public class PasswordExpositionBehavior : Behavior<PasswordBox>
+    {
+        public static readonly DependencyProperty PasswordProperty = DependencyProperty.Register(nameof(Password), typeof(SecureString), typeof(PasswordExpositionBehavior));
+
+        public SecureString Password
+        {
+            get => (SecureString)GetValue(PasswordProperty);
+
+            set => SetValue(PasswordProperty, value);
+        }
+
+        protected override void OnAttached()
+        {
+            AssociatedObject.PasswordChanged += OnPasswordChanged;
+        }
+
+        private void OnPasswordChanged(object sender, RoutedEventArgs e)
+        {
+            BindingExpression binding = BindingOperations.GetBindingExpression(this, PasswordProperty);
+
+            binding?.DataItem
+                    .GetType()
+                    .GetProperty(binding.ParentBinding.Path.Path)
+                    ?.SetValue(binding.DataItem, AssociatedObject.SecurePassword);
+        }
+    }
+}
