c923b36ea8a53a2b67b4b1ace9f9002d495eb83d
Aleksbgbg <kozichka01@abv.bg>
2018-08-30 21:47:48 +0100

    [SessionController] Add 'Register' handler which adds a new user to the database based on form data.

--- Skype v2.0 HTTP Server/Controllers/SessionController.cs
+++ Skype v2.0 HTTP Server/Controllers/SessionController.cs
@@ -1,10 +1,15 @@
 ﻿namespace HttpServer.Controllers
 {
     using System;
+    using System.Text;
+    using System.Threading.Tasks;
 
+    using HttpServer.Database;
     using HttpServer.Services.Interfaces;
 
     using Microsoft.AspNetCore.Mvc;
+
+    using Shared.Models;
 
     [ApiController]
     [Route("[Controller]")]
@@ -12,9 +17,12 @@
     {
         private readonly IAuthService _authService;
 
-        public SessionController(IAuthService authService)
+        private readonly Skype2Context _databaseContext;
+
+        public SessionController(IAuthService authService, Skype2Context databaseContext)
         {
             _authService = authService;
+            _databaseContext = databaseContext;
         }
 
         [HttpGet("login")]
@@ -28,6 +36,25 @@
             string[] authParameters = authorization.Split(' ', 3);
 
             if (_authService.Authorize(authParameters[1], authParameters[2], out string token))
+            {
+                return Ok(token);
+            }
+
+            return Unauthorized();
+        }
+
+        [HttpPost("register")]
+        [Consumes("application/x-www-form-urlencoded")]
+        public async Task<IActionResult> Register([FromForm] string username, [FromForm] string password)
+        {
+            await _databaseContext.Users.AddAsync(new User
+            {
+                Name = username,
+                Password = Encoding.UTF8.GetString(Convert.FromBase64String(password))
+            });
+            await _databaseContext.SaveChangesAsync();
+
+            if (_authService.Authorize(username, password, out string token))
             {
                 return Ok(token);
             }
