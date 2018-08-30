4e7a9e67f042b099871b95338c5cf7eedd9ffe5b
Aleksbgbg <kozichka01@abv.bg>
2018-08-30 22:06:32 +0100

    [MainViewModel] Inject RegisterViewModel, and subscribe to switch events to switch views appropriately.

--- Skype v2.0/ViewModels/MainViewModel.cs
+++ Skype v2.0/ViewModels/MainViewModel.cs
@@ -6,20 +6,58 @@
 
     internal sealed class MainViewModel : Conductor<IViewModelBase>, IMainViewModel
     {
-        public MainViewModel(ILoginViewModel loginViewModel, IChatViewModel chatViewModel)
+        public MainViewModel(ILoginViewModel loginViewModel, IRegisterViewModel registerViewModel, IChatViewModel chatViewModel)
         {
             LoginViewModel = loginViewModel;
+            RegisterViewModel = registerViewModel;
             ChatViewModel = chatViewModel;;
 
             ScreenExtensions.TryActivate(this);
 
             ActivateItem(LoginViewModel);
 
-            LoginViewModel.LoggedIn += (sender, e) => ActivateItem(ChatViewModel);
+            LoginViewModel.LoggedIn += LoggedIn;
+            RegisterViewModel.Registered += Registered;
+
+            LoginViewModel.SwitchToRegisterRequested += SwitchToRegister;
+            RegisterViewModel.SwitchToLoginRequested += SwitchToLogin;
+        }
+
+        private void LoggedIn(object sender, System.EventArgs e)
+        {
+            OnLoggedIn();
+        }
+
+        private void Registered(object sender, System.EventArgs e)
+        {
+            OnLoggedIn();
+        }
+
+        private void SwitchToRegister(object sender, System.EventArgs e)
+        {
+            ActivateItem(RegisterViewModel);
+        }
+
+        private void SwitchToLogin(object sender, System.EventArgs e)
+        {
+            ActivateItem(LoginViewModel);
         }
 
         public ILoginViewModel LoginViewModel { get; }
 
+        public IRegisterViewModel RegisterViewModel { get; }
+
         public IChatViewModel ChatViewModel { get; }
+
+        private void OnLoggedIn()
+        {
+            ActivateItem(ChatViewModel);
+
+            LoginViewModel.LoggedIn -= LoggedIn;
+            RegisterViewModel.Registered -= Registered;
+
+            LoginViewModel.SwitchToRegisterRequested -= SwitchToRegister;
+            RegisterViewModel.SwitchToLoginRequested -= SwitchToLogin;
+        }
     }
 }
