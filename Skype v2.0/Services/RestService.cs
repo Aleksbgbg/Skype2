fd1943966d7cf0cb1a9ef61af39f6720664c93eb
Aleksbgbg <kozichka01@abv.bg>
2018-08-31 15:15:49 +0100

    [RestService] Add BaseAddress for HttpClient, instead of using GetUrl method.

--- Skype v2.0/Services/RestService.cs
+++ Skype v2.0/Services/RestService.cs
@@ -18,14 +18,13 @@
 
     internal class RestService : IRestService
     {
-        private static readonly string BaseRestAddress = $"http://{Constants.ServerIp}:{Constants.HttpPort}/";
-
         private readonly HttpClient _httpClient = new HttpClient();
 
         public string AuthToken { get; private set; }
 
         public RestService()
         {
+            _httpClient.BaseAddress = new Uri($"http://{Constants.ServerIp}:{Constants.HttpPort}/");
             _httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36");
         }
 
@@ -33,7 +32,7 @@
 
         public async Task Login(string username, SecureString password)
         {
-            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, GetUrl("session/login"))
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "session/login")
             {
                 Headers =
                 {
@@ -50,7 +49,7 @@
 
         public async Task Register(string username, SecureString password)
         {
-            HttpResponseMessage response = await _httpClient.PostAsync(GetUrl("session/register"), new FormUrlEncodedContent(new[]
+            HttpResponseMessage response = await _httpClient.PostAsync("session/register", new FormUrlEncodedContent(new[]
             {
                     new KeyValuePair<string, string>("username", username),
                     new KeyValuePair<string, string>("password", HashPassword(password))
@@ -63,7 +62,7 @@
 
         public async Task Logout()
         {
-            await _httpClient.GetAsync(GetUrl("session/logout"));
+            await _httpClient.GetAsync("session/logout");
         }
 
         public async Task<Message[]> GetMessages()
@@ -73,7 +72,7 @@
 
         public async Task<T> Get<T>(string path)
         {
-            HttpResponseMessage response = await _httpClient.GetAsync(GetUrl(path));
+            HttpResponseMessage response = await _httpClient.GetAsync(path);
 
             string content = await response.Content.ReadAsStringAsync();
 
@@ -98,11 +97,6 @@
             string hashString = BitConverter.ToString(hashBytes).Replace("-", "");
 
             return Convert.ToBase64String(Encoding.UTF8.GetBytes(hashString));
-        }
-
-        private static string GetUrl(string target)
-        {
-            return string.Concat(BaseRestAddress, target);
         }
     }
 }
