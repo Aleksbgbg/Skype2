c6d00d6f88db9926f72f309de97eabdd29891415
Aleksbgbg <kozichka01@abv.bg>
2018-09-03 07:50:25 +0100

    [ServiceCollectionExtensions] Add extension to register JWT authentication to the services container.

---
+++ Skype v2.0 HTTP Server/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿namespace HttpServer.Extensions
+{
+    using System.Text;
+
+    using Microsoft.AspNetCore.Authentication.JwtBearer;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.IdentityModel.Tokens;
+
+    using Shared.Config;
+
+    internal static class ServiceCollectionExtensions
+    {
+        internal static void AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            string selfAddress = $"{Constants.ServerIp}:{Constants.TcpPort}";
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = selfAddress,
+                        ValidAudience = selfAddress,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["SecurityKey"]))
+                    });
+        }
+    }
+}
