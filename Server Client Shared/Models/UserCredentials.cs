468db8bcfe001476da161f168ca03018a5dff0a0
Aleksbgbg <kozichka01@abv.bg>
2018-09-03 07:40:38 +0100

    [UserCredentials] Create class which stores a user's Username and Password.

---
+++ Server Client Shared/Models/UserCredentials.cs
@@ -0,0 +1,20 @@
+﻿namespace Shared.Models
+{
+    using Newtonsoft.Json;
+
+    public class UserCredentials
+    {
+        [JsonConstructor]
+        public UserCredentials(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+
+        [JsonProperty("Username")]
+        public string Username { get; }
+
+        [JsonProperty("Password")]
+        public string Password { get; }
+    }
+}
