f1c4a0aec5679d345a47b288b04f466d203eff84
Aleksbgbg <kozichka01@abv.bg>
2018-09-05 18:51:24 +0100

    [ActiveUserRequirement] Add empty requirement that ensures users can only edit their own data.

---
+++ Skype v2.0 HTTP Server/Authorization/Requirements/ActiveUserRequirement.cs
@@ -0,0 +1,9 @@
+﻿namespace HttpServer.Authorization.Requirements
+{
+    using Microsoft.AspNetCore.Authorization;
+
+    internal class ActiveUserRequirement : IAuthorizationRequirement
+    {
+        // Ensures that a user can only edit their personal data, and not other users'
+    }
+}
