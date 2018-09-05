bd2525d11e8fe4128d1099da8c51b8116a8d1350
Aleksbgbg <kozichka01@abv.bg>
2018-09-05 18:52:18 +0100

    [ActiveUserHandler] Add handler for ActiveUserRequirement, which checks whether the logged-in user is the target user.

---
+++ Skype v2.0 HTTP Server/Authorization/Handlers/ActiveUserHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace HttpServer.Authorization.Handlers
+{
+    using System.Threading.Tasks;
+
+    using HttpServer.Authorization.Requirements;
+
+    using Microsoft.AspNetCore.Authorization;
+
+    using Shared.Models;
+
+    internal class ActiveUserHandler : AuthorizationHandler<ActiveUserRequirement, User>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ActiveUserRequirement requirement, User targetUser)
+        {
+            if (context.User.Identity.Name == targetUser.Name)
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
