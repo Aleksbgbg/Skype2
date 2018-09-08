4a4d1d226ce6a360e2e8870554c82327117ef87d
Aleksbgbg <kozichka01@abv.bg>
2018-09-08 19:11:05 +0100

    [Models] Add ClientSession and Session models which contain tokens and refresh tokens with expiration dates.

---
+++ Server Client Shared/Models/ClientSession.cs
@@ -0,0 +1,18 @@
+﻿namespace Shared.Models
+{
+    using System;
+
+    using Newtonsoft.Json;
+
+    public class ClientSession
+    {
+        [JsonProperty("Token")]
+        public string Token { get; set; }
+
+        [JsonProperty("ExpiresAt")]
+        public DateTime ExpiresAt { get; set; }
+
+        [JsonProperty("Session")]
+        public Session Session { get; set; }
+    }
+}
---
+++ Server Client Shared/Models/Session.cs
@@ -0,0 +1,15 @@
+﻿namespace Shared.Models
+{
+    using System;
+
+    using Newtonsoft.Json;
+
+    public class Session
+    {
+        [JsonProperty("RefreshToken")]
+        public string RefreshToken { get; set; }
+
+        [JsonProperty("ExpiresAt")]
+        public DateTime ExpiresAt { get; set; }
+    }
+}
